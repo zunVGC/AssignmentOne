8f223cfad0cb64d823b4ffd6316ccb22dcc6690f
zunVGC <y_1st.zenith@icloud.com>
2019-09-24 20:14:56 +0800

    A1

---
+++ AssignmentOne/Assets/Create.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour
+{
+    public Transform o1;
+    public Rigidbody o2;
+
+    void CreateBall()
+    {
+        Rigidbody o3;
+        o3 = Instantiate(o2, o1.position, o1.rotation)as Rigidbody;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            CreateBall();
+        }
+    }
+
+}
---
+++ AssignmentOne/Assets/SimplePluginTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class SimplePluginTest : MonoBehaviour
+{
+    const string DLL_NAME = "PROJECT1";
+    [DllImport(DLL_NAME)]
+    private static extern int SimpleFunction();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(SimpleFunction());
+        }
+    }
+}
