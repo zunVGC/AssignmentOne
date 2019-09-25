e152f8100d1c82ed473dfd44040f01112b18b23c
zunVGC <y_1st.zenith@icloud.com>
2019-09-26 02:09:35 +0800

    A1 redo&spawn

--- AssignmentOne/Assets/Script/Create.cs
+++ AssignmentOne/Assets/Script/Create.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Create : MonoBehaviour
 {
     public Transform o1;
-    public Rigidbody o2;
+    public Transform o2;
 
     void CreateBall()
     {
-        Rigidbody o3;
-        o3 = Instantiate(o2, o1.position, o1.rotation)as Rigidbody;
+        Transform o3 = GameObject.Instantiate(o2, o1.position, o1.rotation);
     }
 
     void Update()
